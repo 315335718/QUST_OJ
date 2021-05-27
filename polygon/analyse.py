e7f912fd6701683491de1c7f5bc4843a7a9e4471
Jk Xu <54522439+Dousir9@users.noreply.github.com>
2021-05-27 23:05:35 +0800

    更新类题目添加delete和insert评测

--- polygon/analyse.py
+++ polygon/analyse.py
@@ -7,7 +7,20 @@ def get_table(type, description, checker):
     if type == '查询类':
         result['other'] = ''
     elif type == '更新类':
-        table_to_select = re.findall(r'\s*update\s+(\S+)\s+', checker, re.I)
+        # table_to_select = re.findall(r'\s*update\s+(\S+)\s+', checker, re.I)
+        cur = re.findall(r'\s*update\s+(\S+)\s+', checker, re.I)
+        if len(cur) > 0:
+            table_to_select = cur
+        else:
+            cur = re.findall(r'\s*delete\s+from\s+(\S+)\s+', checker, re.I)
+            if len(cur) > 0:
+                table_to_select = cur
+            else:
+                cur = re.findall(r'\s*insert\s+into\s+(\S+)\s*\(', checker, re.I)
+                if len(cur) > 0:
+                    table_to_select = cur
+                else:
+                    table_to_select = re.findall(r'\s*insert\s+into\s+(\S+)\s+', checker, re.I)
         result['other'] = table_to_select
     elif type == '创建视图类':
         view_to_select = re.findall(r'\s*create\s+view\s+(\S+)\s*', checker, re.I)
