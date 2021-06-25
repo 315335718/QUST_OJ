5a737f288a31c16d38b407cf79a0036d38d669f2
Jk Xu <54522439+Dousir9@users.noreply.github.com>
2021-06-25 20:54:32 +0800

    修复了创建视图类题目时存在的缺陷

--- polygon/analyse.py
+++ polygon/analyse.py
@@ -5,9 +5,20 @@ def get_table(type, description, checker):
     table_to_delete = re.findall(r'\s*create\s+table\s+(\S+)\s*\(', description, re.I)
     result['table_to_delete'] = table_to_delete
     if type == '查询类':
-        result['other'] = ''
+        view_to_select = []
+        view1 = re.findall(r'\s*create\s+view\s+(\S+)\s*', description, re.I)
+        view2 = re.findall(r'\s*create\s+view\s+(\S+)\(', description, re.I)
+        if len(view1) > 0 and len(view2) > 0:
+            if len(view1[0]) < len(view2[0]):
+                view_to_select = view1
+            else:
+                view_to_select = view2
+        elif len(view1) > 0:
+            view_to_select = view1
+        else:
+            view_to_select = view2
+        result['other'] = view_to_select
     elif type == '更新类':
-        # table_to_select = re.findall(r'\s*update\s+(\S+)\s+', checker, re.I)
         cur = re.findall(r'\s*update\s+(\S+)\s+', checker, re.I)
         if len(cur) > 0:
             table_to_select = cur
@@ -21,6 +32,21 @@ def get_table(type, description, checker):
                     table_to_select = cur
                 else:
                     table_to_select = re.findall(r'\s*insert\s+into\s+(\S+)\s+', checker, re.I)
+        # 视图
+        view_to_select = []
+        view1 = re.findall(r'\s*create\s+view\s+(\S+)\s*', description, re.I)
+        view2 = re.findall(r'\s*create\s+view\s+(\S+)\(', description, re.I)
+        if len(view1) > 0 and len(view2) > 0:
+            if len(view1[0]) < len(view2[0]):
+                view_to_select = view1
+            else:
+                view_to_select = view2
+        elif len(view1) > 0:
+            view_to_select = view1
+        else:
+            view_to_select = view2
+        table_to_select += view_to_select
+
         result['other'] = table_to_select
     elif type == '创建视图类':
         view_to_select = []
