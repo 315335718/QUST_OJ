a2845847dfcaea00f614d7fb2546742499ce45d6
Jk Xu <54522439+Dousir9@users.noreply.github.com>
2021-07-21 01:34:06 +0800

    修复了创建视图类题目对其他类题目产生的影响

--- polygon/analyse.py
+++ polygon/analyse.py
@@ -6,18 +6,18 @@ def get_table(type, description, checker):
     result['table_to_delete'] = table_to_delete
     if type == '查询类':
         view_to_select = []
-        view1 = re.findall(r'\s*create\s+view\s+(\S+)\s*', description, re.I)
-        view2 = re.findall(r'\s*create\s+view\s+(\S+)\(', description, re.I)
-        if len(view1) > 0 and len(view2) > 0:
-            if len(view1[0]) < len(view2[0]):
-                view_to_select = view1
-            else:
-                view_to_select = view2
-        elif len(view1) > 0:
-            view_to_select = view1
-        else:
-            view_to_select = view2
-        result['other'] = view_to_select
+        # view1 = re.findall(r'\s*create\s+view\s+(\S+)\s*', description, re.I)
+        # view2 = re.findall(r'\s*create\s+view\s+(\S+)\(', description, re.I)
+        # if len(view1) > 0 and len(view2) > 0:
+        #     if len(view1[0]) < len(view2[0]):
+        #         view_to_select = view1
+        #     else:
+        #         view_to_select = view2
+        # elif len(view1) > 0:
+        #     view_to_select = view1
+        # else:
+        #     view_to_select = view2
+        # result['other'] = view_to_select
     elif type == '更新类':
         cur = re.findall(r'\s*update\s+(\S+)\s+', checker, re.I)
         if len(cur) > 0:
@@ -32,20 +32,20 @@ def get_table(type, description, checker):
                     table_to_select = cur
                 else:
                     table_to_select = re.findall(r'\s*insert\s+into\s+(\S+)\s+', checker, re.I)
-        # 视图
-        view_to_select = []
-        view1 = re.findall(r'\s*create\s+view\s+(\S+)\s*', description, re.I)
-        view2 = re.findall(r'\s*create\s+view\s+(\S+)\(', description, re.I)
-        if len(view1) > 0 and len(view2) > 0:
-            if len(view1[0]) < len(view2[0]):
-                view_to_select = view1
-            else:
-                view_to_select = view2
-        elif len(view1) > 0:
-            view_to_select = view1
-        else:
-            view_to_select = view2
-        table_to_select += view_to_select
+        # # 视图
+        # view_to_select = []
+        # view1 = re.findall(r'\s*create\s+view\s+(\S+)\s*', description, re.I)
+        # view2 = re.findall(r'\s*create\s+view\s+(\S+)\(', description, re.I)
+        # if len(view1) > 0 and len(view2) > 0:
+        #     if len(view1[0]) < len(view2[0]):
+        #         view_to_select = view1
+        #     else:
+        #         view_to_select = view2
+        # elif len(view1) > 0:
+        #     view_to_select = view1
+        # else:
+        #     view_to_select = view2
+        # table_to_select += view_to_select
 
         result['other'] = table_to_select
     elif type == '创建视图类':
